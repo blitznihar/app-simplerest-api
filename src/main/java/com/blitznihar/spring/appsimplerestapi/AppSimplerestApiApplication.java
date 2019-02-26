787730724563570b7e4be50ff431520d97d45429
blitznihar <blitznihar@gmail.com>
2019-02-26 01:12:52 -0600

    Init

---
+++ src/main/java/com/blitznihar/spring/appsimplerestapi/AppSimplerestApiApplication.java
@@ -0,0 +1,13 @@
+package com.blitznihar.spring.appsimplerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSimplerestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSimplerestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blitznihar/spring/appsimplerestapi/AppSimplerestApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.blitznihar.spring.appsimplerestapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppSimplerestApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
